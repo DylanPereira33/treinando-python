b2c4c19b38fb1bfc34613412fd11d0a65aadc62d
Dylan Pereira <153128056+DylanPereira33@users.noreply.github.com>
2024-01-15 18:51:56 -0300

    Update capitulo 6/7-Pessoas2.py
    Co-authored-by: Luke Dias <51720084+LukeDias42@users.noreply.github.com>

--- capitulo 6/7-Pessoas2.py
+++ capitulo 6/7-Pessoas2.py
@@ -2,7 +2,7 @@ person_0 = {'first_name':'Derick','last_name':'Pereira','age':14,'city':'Rio de 
 person_1 = {'first_name':'Dylan','last_name':'Pereira','age':18,'city':'Rio de Janeiro'}
 person_2 = {'first_name':'Luke','last_name':'Dias','age':24,'city':'Rio de Janeiro'}
 
-people = (person_0,person_1,person_2)
+people = [person_0,person_1,person_2]
 
 for person in people:
     for k,v in person.items():
