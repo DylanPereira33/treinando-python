db616b78ec8926ac71659c1f4bbf294c6ea3ee99
Gbodq <dylanfranca@outlook.com>
2024-02-21 13:55:11 -0300

    Co-authored-by: Luke Dias <LukeDias42@users.noreply.github.com> mudança e melhorias no codigo labyrinth

--- desafios/labyrinth.py
+++ desafios/labyrinth.py
@@ -1,63 +1,77 @@
-mapa = """------------
-|.#........|
-|..###..#..|
-|.....#..#.|
-|.####.#P#.|
-|.......#..|
-|#.####...#|
-|#.....#.##|
-|.###...#..|
-|.#...#.#.#|
-|c..#.....#|
-------------"""
+import time
+import os
+
+mapa = """------------------
+|.#........#..#..|
+|..###..#.....#..|
+|.....#..##.....#|
+|.####.#P#.###...|
+|.......#...#...#|
+|#.####...#.#..#.|
+|#.....#.##.#..#.|
+|.###...#......#.|
+|.....#.###.#.#..|
+|.#.#.#...#.#....|
+|.#.#.#...###.##.|
+|.#...#.#.#.#....|
+|c..#.....#....##|
+------------------"""
 
 # Transformar o mapa em uma lista de strings
-mapa = mapa.split('\n')
-
 # Localização inicial do personagem 'c'
-posicao_c = 0
-posicao_p = 0
+posicao_c = mapa.index('c')
+posicao_p = mapa.index('P')
 
-for i, linha in enumerate(mapa):
-    if 'c' in linha:
-        posicao_c = (i, linha.index('c'))
-    if 'P' in linha:
-        posicao_p = (i, linha.index('P'))
+def desenhaMapa():
+    os.system('clear')
+    print(mapa)
 
-# Função para imprimir o mapa
-def imprimir_mapa():
-    for linha in mapa:
-        print(''.join(linha))
-
-# Loop para mover o personagem até o ponto final 'P'
+colunas = len(mapa.split("\n")[0])+1
+desenhaMapa()
+inputs = ['w', 'a', 's', 'd']
+startTime = 0
 while  posicao_c != posicao_p:
-    imprimir_mapa()
-    comando = input("Digite um comando (w, s, a, d): ")
-    
-    # Atualizar a posição do personagem de acordo com o comando
-    if comando == 'w':  # mover para cima
-        nova_posicao = (posicao_c[0] - 1, posicao_c[1])
-    elif comando == 's':  # mover para baixo
-        nova_posicao = (posicao_c[0] + 1, posicao_c[1])
-    elif comando == 'a':  # mover para esquerda
-        nova_posicao = (posicao_c[0], posicao_c[1] - 1)
-    elif comando == 'd':  # mover para direita
-        nova_posicao = (posicao_c[0], posicao_c[1] + 1)
-    else:
-        print("Comando inválido. Use w, s, a ou d.")
-        
-    # Verificar se a nova posição é válida (não ultrapassa os limites do mapa e não é um obstáculo)
-    if mapa[nova_posicao[0]][nova_posicao[1]] != '#':
-      
-    # Atualizar o mapa com a nova posição do personagem 'c'
-        mapa[posicao_c[0]] = mapa[posicao_c[0]].replace('c', '.')  # Substitui a posição anterior por um espaço vazio
-        mapa[nova_posicao[0]] = mapa[nova_posicao[0]][:nova_posicao[1]] + 'c' + mapa[nova_posicao[0]][nova_posicao[1] + 1:]
+    comandos = input("Digite um comando (w, s, a, d): ")
+    hasError = False
+    for comando in comandos:
+        if comando not in inputs:
+            desenhaMapa()
+            print("Algum comando inválido. Use w, s, a ou d.")
+            hasError = True
+            break
+
+    if hasError:
+        continue
+    for comando in comandos:
+        # Atualizar a posição do personagem de acordo com o comando
+        if comando[0] == 'w':  # mover para cima
+            nova_posicao = posicao_c - colunas
+        elif comando[0] == 's':  # mover para baixo
+            nova_posicao = posicao_c + colunas
+        elif comando[0] == 'a':  # mover para esquerda
+            nova_posicao = posicao_c - 1
+        elif comando[0] == 'd':  # mover para direita
+            nova_posicao = posicao_c + 1
+
+        if startTime == 0:
+            startTime = time.perf_counter()
+
+        place = mapa[nova_posicao]
+        # Verificar se a nova posição é válida (não ultrapassa os limites do mapa e não é um obstáculo)
+        if place == '#'or place == "|"or place == "-":
+            desenhaMapa()
+            print("Movimento inválido. Tente novamente.")
+            break
+
+        # Atualizar o mapa com a nova posição do personagem 'c'
+        mapa = mapa.replace('c', '.')  # Substitui a posição anterior por um espaço vazio
+        mapa = mapa[:nova_posicao] + 'c' + mapa[nova_posicao + 1:]
         posicao_c = nova_posicao
+        desenhaMapa()
 
-    else:
-        print("Movimento inválido. Tente novamente.")
-
-print("Parabéns! Você chegou ao ponto final 'P'.")
+endTime = time.perf_counter()
+elapsedTime = endTime - startTime
+print(f"Parabéns! Você chegou ao objetivo. {elapsedTime:.2f}s")
 
 
 
