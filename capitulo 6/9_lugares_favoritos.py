f23e6842cb4ee424be2f3bdafb7fb79585311b06
Gbodq <dylanfranca@outlook.com>
2024-04-02 14:58:53 -0300

    Co-authored-by: Luke Dias <LukeDias42@users.noreply.github.com>

--- capitulo 6/9_lugares_favoritos.py
+++ capitulo 6/9_lugares_favoritos.py
@@ -1,6 +1,12 @@
-favorite_places = {"Dylan":["Japão","Alemanha"],"Derick":["Brasil","França"],"Luke":["Portugal","Inglaterra"] }
+favorite_places = {"Dylan":["Japão"],"Derick":["Brasil","França"],"Luke":["Portugal","Inglaterra", "Itália", "Holanda", "Hungria"] }
 
 for nome,lugares in favorite_places.items():
-    print(f"O lugar favorito do {nome} é ")
-    for lugar in lugares:
-        print("\t" + lugar)
+    if len(lugares) == 0:
+        print(f"{nome} gosta de ficar em casa")
+    elif len(lugares) == 1:
+        print(f"O lugar preferido do usuário {nome} é {lugares[0]}")
+    else:
+        *inicio, ultimo = lugares
+        print(f"Os lugares favoritos do usuário {nome} são: ", end= "")
+        print(*inicio, sep=", ",end=" e ")
+        print(ultimo)
