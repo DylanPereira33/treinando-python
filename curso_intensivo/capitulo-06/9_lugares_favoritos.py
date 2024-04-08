1fa3bfdea75379ab033451b18fbb5a974d814d1d
DylanPereira33 <dylanfranca@outlook.com>
2024-04-07 21:36:19 -0300

    corrigindo erros no código

--- curso_intensivo/capitulo-06/9_lugares_favoritos.py
+++ curso_intensivo/capitulo-06/9_lugares_favoritos.py
@@ -1,6 +1,14 @@
-favorite_places = {"Dylan":["Japão","Alemanha"],"Derick":["Brasil","França"],"Luke":["Portugal","Inglaterra"] }
+favorite_places = {"Dylan":["Japão","Alemanha"],"Derick":["Brasil",],"Luke":["Portugal","Inglaterra","França","Alemanha"] }
 
 for nome,lugares in favorite_places.items():
-    print(f"O lugar favorito do {nome} é ")
-    for lugar in lugares:
-        print("\t" + lugar)
+    if len(lugares) == 0:
+        print(f"{nome} não gosta de lugares")
+    if len(lugares) == 1:
+        print(f"O lugar favorito do {nome} é {lugares[0]}")
+    if len(lugares) > 1:
+        *inicio, ultimo = lugares
+        print(f"Os lugares favoritos do {nome} são: ", end="")
+        print(*inicio, sep=", ", end= " e ")
+        print(ultimo)
+    
+    
